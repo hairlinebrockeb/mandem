81791b9cd4b3515795a7ee351a5530a7afd3ee0b
hairlinebrockeb <47501906+hairlinebrockeb@users.noreply.github.com>
2022-10-15 19:17:27 +0100

    Update decodeALL.lua

--- decodeALL.lua
+++ decodeALL.lua
@@ -1,6 +1,21 @@
-local x = {}
+-- local x = {}
 
-x.code = function(data)
+-- x.code = function(data)
+--     local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+--     data = string.gsub(data, '[^'..b..'=]', '')
+--     return (data:gsub('.', function(x)
+--         if (x == '=') then return '' end
+--         local r,f='',(b:find(x)-1)
+--         for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+--         return r;
+--     end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+--         if (#x ~= 8) then return '' end
+--         local c=0
+--         for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+--         return string.char(c)
+--     end))
+-- end
+local d = function(data)
     local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
     data = string.gsub(data, '[^'..b..'=]', '')
     return (data:gsub('.', function(x)
@@ -15,4 +30,4 @@ x.code = function(data)
         return string.char(c)
     end))
 end
-return x 
+return d
